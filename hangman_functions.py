01903b1afd7d3e34841e72d0935e5c26fc1bd720
TomerBitonn <168045524+TomerBitonn@users.noreply.github.com>
2024-04-25 04:31:46 +0300

    Add files via upload

--- hangman_functions.py
+++ hangman_functions.py
@@ -9,20 +9,12 @@ def hangman_open_screen():
 
 
 def print_hangman(num_of_tries):
-    """prints the hangman position"""
+    #prints the hangman position
     print(constants.HANGMAN_PHOTOS[num_of_tries])
-    return
     
     
 def choose_word(file_path, index):
-    """Choosing a secret word from a text file.
-    :file_path: path of the text file
-    :index: index of the word from the file
-    :file_path type: str 
-    :index type: int 
-    :return: secret word
-    :rtype: tuple 
-    """
+    #choosing the secret word from the text file
     with open(file_path, "r") as words_list: #open the file as a new file named "words list"
         words = words_list.read() #read the file "words list" and define it as a new file
         words_into_list = words.replace('\n', ' ').split(' ') #replace the new lines with spaces and make it a list
@@ -40,31 +32,15 @@ def choose_word(file_path, index):
     
     
 def show_hidden_word(secret_word, old_letters_guessed):
-    """The function recieve a word and a list of letters,
-    ant return a word with a letters from the list.
-    :secret_word: the word the player need to guess
-    :old_letters_guessed: list of the letters the player guessed
-    :secret_word type: str
-    :old_letters_guessed type: list
-    :return: a word with letters from the list 
-    :rtype: str 
-    """
+    #show everytime one letter from the secret word
     for letter in secret_word:
         if letter not in old_letters_guessed:
             secret_word = secret_word.replace(letter, " _ ")
-    return print(secret_word)
+    print(secret_word)
     
     
 def try_update_letter_guessed(letter_guessed, old_letters_guessed, secret_word):
-    """If the recieved value is ok, it will be added to 
-    the old_letters_guessed list. If not, the function will print X.
-    :letter_guessed: recievd value from the player
-    :old_letters_guessed: list with all of the guesswork
-    :letter_guessed type: str
-    :old_letters_guessed type: list
-    :return: True, False and list of guessed letters
-    :rtype: bool, str, list 
-    """
+    #check the letter guessed
     if (len(letter_guessed) == 1) and letter_guessed.isalpha() \
     and (letter_guessed.lower() not in old_letters_guessed):
         check_letter = True
@@ -79,14 +55,7 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed, secret_word):
 
 
 def check_win(secret_word, old_letters_guessed):
-    """This function check if the player won or not.
-    :secret_word: word the player need to guess
-    :old_letters_guessed: letters the player guessed
-    :secret_word type: str
-    :old_letters_guessed type: list
-    :return: True or False
-    :rtype: bool
-    """
+    #check if the player won
     counter = i = j = 0 
     for i in range(len(secret_word)):
         for j in range(len(old_letters_guessed)):
